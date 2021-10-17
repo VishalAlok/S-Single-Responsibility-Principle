d1cf474820e30f28ba7c80a8234b50df26cdea48
vishal.Alok <you@example.com>
2021-10-18 02:49:45 +0530

    Add project files.

---
+++ iUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Single_Responsibility_Principle
+{
+    interface iUser
+    {
+        bool login(string name, string password);
+        bool Register(string name, string OTP,string EmailID);
+    }
+    interface ilogged
+    {
+        bool LogError(string ExceptionMsg);
+    }
+    interface iEmail
+    {
+        bool LogError(string ExceptionMsg);
+    }
+}
